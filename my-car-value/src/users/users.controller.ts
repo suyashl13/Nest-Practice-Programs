7f1db6b2e3f194273234cf6205a044b0b0a946e3
Suyash Lawand (Mac) <suyash.lawand@gmail.com>
2023-09-15 20:25:36 +0530

    add: new app my-car-value

---
+++ my-car-value/src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Controller('auth')
+export class UsersController {
+    constructor(){}
+
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto){
+        console.log(body);
+    }
+}

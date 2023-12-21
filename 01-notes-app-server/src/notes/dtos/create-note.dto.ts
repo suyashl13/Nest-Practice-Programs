8810d965a78b8b1486a1cd6a86e37af19ab7610c
Suyash Lawand (Mac) <suyash.lawand@gmail.com>
2023-12-21 10:33:51 +0530

    restart: fresh start

--- 01-notes-app-server/src/notes/dtos/create-note.dto.ts
+++
@@ -1,6 +0,0 @@
-import { IsString } from 'class-validator'
-
-export class CreateNoteDto {
-    @IsString()
-    note: string;
-}
--- my-car-value/src/users/guards/auth.guards.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
-
-export class AuthGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean {
-        const request = context.switchToHttp().getRequest();
-        console.log(request.session.userId);
-        return Boolean(request.session.userId);
-    }
-}
